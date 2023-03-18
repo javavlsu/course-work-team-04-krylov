f3b7fcc1cb64c91639e0205b668ba19b41945993
TopRuiner <krylov.em2002@gmail.com>
2023-03-19 01:17:57 +0300

    update и create для диагноза

--- project/MIS/src/main/java/com/polyclinic/mis/controllers/DiagnosisController.java
+++ project/MIS/src/main/java/com/polyclinic/mis/controllers/DiagnosisController.java
@@ -1,13 +1,13 @@
 package com.polyclinic.mis.controllers;
 
 import com.polyclinic.mis.models.Diagnosis;
-import com.polyclinic.mis.service.DiagnosisService;
 import com.polyclinic.mis.service.impl.DiagnosisServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class DiagnosisController {
@@ -18,5 +18,27 @@ public class DiagnosisController {
         Iterable<Diagnosis> diagnoses = diagnosisService.getAll();
         model.addAttribute("diagnoses",diagnoses);
         return "/Diagnoses/Index";
+    }
+    @GetMapping("/Diagnoses/Create")
+    public String ShowCreate(Model model){
+        Diagnosis diagnosis = new Diagnosis();
+        model.addAttribute("diagnosis",diagnosis);
+        return "/Diagnoses/Create";
+    }
+    @PostMapping("/Diagnoses/Create")
+    public String Create(@ModelAttribute("diagnosis")Diagnosis diagnosis){
+        diagnosisService.add(diagnosis);
+        return "redirect:/Diagnoses";
+    }
+    @GetMapping("Diagnoses/Edit/{id}")
+    public String ShowEdit(@PathVariable (value = "id") String id, Model model){
+        Optional<Diagnosis> diagnosis = diagnosisService.getById(id);
+        if (diagnosis.isPresent()){
+            model.addAttribute("diagnosis",diagnosis);
+            return "/Diagnoses/Update";
+        }
+        else {
+            return "/Error";
+        }
     }
 }
