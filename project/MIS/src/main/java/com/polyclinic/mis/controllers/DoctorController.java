08db807c8c006a6cf780b31575b367ba958ea67c
TopRuiner <krylov.em2002@gmail.com>
2023-04-16 14:25:15 +0300

    Добавлена регистрация по ролям

--- project/MIS/src/main/java/com/polyclinic/mis/controllers/DoctorController.java
+++ project/MIS/src/main/java/com/polyclinic/mis/controllers/DoctorController.java
@@ -1,11 +1,17 @@
 package com.polyclinic.mis.controllers;
 
-import com.polyclinic.mis.models.AnalysisReferral;
-import com.polyclinic.mis.models.Doctor;
-import com.polyclinic.mis.models.DoctorAppointment;
+import com.polyclinic.mis.auth.UserService;
+import com.polyclinic.mis.models.*;
+import com.polyclinic.mis.repository.RoleRepository;
 import com.polyclinic.mis.service.impl.AnalysisReferralServiceImpl;
 import com.polyclinic.mis.service.impl.DoctorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +20,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class DoctorController {
     @Autowired
     DoctorServiceImpl doctorService;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    UserService userService;
+    @Autowired
+    AuthenticationManager authenticationManager;
     @GetMapping("/Doctors")
     public String Index(Model model){
         Iterable<Doctor> doctors = doctorService.getAll();
@@ -35,9 +49,37 @@ public class DoctorController {
     }
     @PostMapping("/Doctors/Create")
     public String Create(@ModelAttribute("doctor")Doctor doctor){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        var user = userService.findUserByEmail(email);
+        doctor.setUser(user);
+
         doctorService.add(doctor);
+
+        assignRole(doctor,user);
         return "redirect:/Doctors";
     }
+
+    private void assignRole(Doctor doctor, PolyclinicUser user){
+        var doctorUser= doctor.getUser();
+        var roles = user.getRoles();
+        var canRegisterAsDoctor = roleRepository.findByName("CanRegisterAsDoctor").get();
+        roles.remove(canRegisterAsDoctor);
+        var doctorRole = roleRepository.findByName("Doctor").get();
+        roles.add(doctorRole);
+        doctorUser.setRoles(roles);
+        userService.updateUser(doctorUser);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        List<GrantedAuthority> updatedAuthorities = new ArrayList<>(auth.getAuthorities());
+        updatedAuthorities.remove(new SimpleGrantedAuthority(canRegisterAsDoctor.getName()));
+        updatedAuthorities.add(new SimpleGrantedAuthority(doctorRole.getName()));
+
+        Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), updatedAuthorities);
+
+        SecurityContextHolder.getContext().setAuthentication(newAuth);
+    }
+
     @GetMapping("Doctors/Edit/{id}")
     public String ShowEdit(@PathVariable(value = "id") long id, Model model){
         Optional<Doctor> doctor = doctorService.getById(id);
--- project/MIS/src/main/java/com/polyclinic/mis/controllers/ReceptionistController.java
+++ project/MIS/src/main/java/com/polyclinic/mis/controllers/ReceptionistController.java
@@ -1,11 +1,20 @@
 package com.polyclinic.mis.controllers;
 
+import com.polyclinic.mis.auth.UserService;
 import com.polyclinic.mis.models.AnalysisReferral;
 import com.polyclinic.mis.models.Patient;
+import com.polyclinic.mis.models.PolyclinicUser;
 import com.polyclinic.mis.models.Receptionist;
+import com.polyclinic.mis.repository.RoleRepository;
 import com.polyclinic.mis.service.impl.FunctionalDiagnosticsDoctorServiceImpl;
 import com.polyclinic.mis.service.impl.ReceptionistServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +22,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -20,6 +31,12 @@ public class ReceptionistController {
 
     @Autowired
     ReceptionistServiceImpl receptionistService;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    UserService userService;
+    @Autowired
+    AuthenticationManager authenticationManager;
     @GetMapping("/Receptionists")
     public String Index(Model model){
         Iterable<Receptionist> receptionists = receptionistService.getAll();
@@ -34,9 +51,36 @@ public class ReceptionistController {
     }
     @PostMapping("/Receptionists/Create")
     public String Create(@ModelAttribute("receptionist")Receptionist receptionist){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        var user = userService.findUserByEmail(email);
+        receptionist.setUser(user);
+
         receptionistService.add(receptionist);
+
+        assignRole(receptionist,user);
         return "redirect:/Receptionists";
     }
+    private void assignRole(Receptionist receptionist, PolyclinicUser user){
+        var receptionistUser= receptionist.getUser();
+        var roles = user.getRoles();
+        var canRegisterAsReceptionist = roleRepository.findByName("CanRegisterAsReceptionist").get();
+        roles.remove(canRegisterAsReceptionist);
+        var receptionistRole = roleRepository.findByName("Receptionist").get();
+        roles.add(receptionistRole);
+        receptionistUser.setRoles(roles);
+        userService.updateUser(receptionistUser);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        List<GrantedAuthority> updatedAuthorities = new ArrayList<>(auth.getAuthorities());
+        updatedAuthorities.remove(new SimpleGrantedAuthority(canRegisterAsReceptionist.getName()));
+        updatedAuthorities.add(new SimpleGrantedAuthority(receptionistRole.getName()));
+
+        Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), updatedAuthorities);
+
+        SecurityContextHolder.getContext().setAuthentication(newAuth);
+    }
+
     @GetMapping("Receptionists/Edit/{id}")
     public String ShowEdit(@PathVariable(value = "id") long id, Model model){
         Optional<Receptionist> receptionist = receptionistService.getById(id);
