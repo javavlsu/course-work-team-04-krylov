7351becfceb1ef9b0a15e7c486621e2d219fa51a
TopRuiner <krylov.em2002@gmail.com>
2023-05-03 18:39:42 +0300

    Update AnalysisRepository.java

--- project/MIS/src/main/java/com/polyclinic/mis/repository/AnalysisRepository.java
+++ project/MIS/src/main/java/com/polyclinic/mis/repository/AnalysisRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -13,8 +14,8 @@ import java.util.List;
 
 @Repository
 public interface AnalysisRepository extends JpaRepository<Analysis,Long> {
-    @Query("SELECT a from Analysis as a where a.patient.lastName like %?1% and a.patient.firstName like %?2% and a.patient.middleName like %?3% and FUNCTION('date_format',a.patient.birthDate,'%Y-%m-%d') like %?4%")
+    @Query("SELECT a from Analysis as a where a.patient.lastName like %:lastName% and a.patient.firstName like %:firstName% and a.patient.middleName like %:middleName% and FUNCTION('date_format',a.patient.birthDate,'%Y-%m-%d') like %:date%")
 
 //    @Query("SELECT a from Analysis as a where a.patient.lastName like %?1%")
-    public Page<Analysis> findAll(String lastName, String firstName, String middleName, String date, Pageable pageable);
+    public Page<Analysis> findAll(@Param("lastName") String lastName, String firstName, String middleName, String date, Pageable pageable);
 }
